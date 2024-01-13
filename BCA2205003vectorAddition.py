963a8144221f8a86b9a7789ecdefb6eaeafcce49
pranaykalg <155800836+pranaykalg@users.noreply.github.com>
2024-01-13 12:32:13 +0530

    code change

--- BCA2205003vectorAddition.py
+++ BCA2205003vectorAddition.py
@@ -1,13 +1,3 @@
-import numpy as np
-
-# Define two arrays
-array1 = np.array([1, 2, 3])
-array2 = np.array([4, 5, 6])
-
-# Add the arrays
-result_array = array1 + array2
-
-# Print the result
-print("Array 1:", array1)
-print("Array 2:", array2)
-print("Resultant Array:", result_array)
+mum=8
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
