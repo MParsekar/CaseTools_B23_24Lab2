84a761fedbb3041442323614ccf0a12597ca1f19
akash97arrow <akashgoudar97@gmail.com>
2024-03-02 08:56:11 +0530

    hii

---
+++ BCA2205119/ex1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ex1 { public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the operation (+, -, *, /):");
+        char operation = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operation!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
