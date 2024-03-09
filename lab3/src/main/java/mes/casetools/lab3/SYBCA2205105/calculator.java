10cc4df535399cac709071257f87f3b51940d870
VLamani <vishallamani063@gmail>
2024-03-09 12:01:58 +0530

    bca2205105

--- lab3/src/main/java/mes/casetools/lab3/SYBCA2205105/calculator.java
+++ lab3/src/main/java/mes/casetools/lab3/SYBCA2205105/calculator.java
@@ -17,7 +17,7 @@ public class calculator {
      */
 
     public int add(int num1 , int num2) {
-        return num1 + num2;  // The Magic happens here!
+        return num1 + num2;  // The Magic happens here!!
 
     }
     
